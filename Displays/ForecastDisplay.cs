20dd507b37cebadb9b3a4db0099551f36a8bbcbf
Ryzex911 <ryzexgamer1@gmail.com>
2026-09-11 11:09:13 +0200

    Week 2 - Observer Pattern

---
+++ Displays/ForecastDisplay.cs
@@ -0,0 +1,50 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Displays
+{
+    internal class ForecastDisplay : Observer, DisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private Subject weatherData;
+        public ForecastDisplay(Subject weatherData) 
+        { 
+            // Set the field and register itself with the weatherdata subject
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+        public void Update(float temp, float humidity, float pressure)
+        {
+            // Set the correct fields with the relevant parameters
+            this.temperature = temp;
+            this.humidity = humidity;
+            Display();
+        }
+
+        public void Display()
+        {
+            // Print a forecast message based on the current temperature and humidity
+            if (temperature > 80 && humidity < 50)
+            {
+                Console.WriteLine("Forecast: It's going to be hot and dry today.");
+            }
+            else if (temperature > 80 && humidity >= 50)
+            {
+                Console.WriteLine("Forecast: It's going to be hot and humid today.");
+            }
+            else if (temperature <= 80 && humidity < 50)
+            {
+                Console.WriteLine("Forecast: It's going to be cool and dry today.");
+            }
+            else
+            {
+                Console.WriteLine("Forecast: It's going to be cool and humid today.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ObserverPattern.Displays;
+
+namespace ObserverPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            // Create instances of displays 
+
+            CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+
+
+
+            weatherData.SetMeasurements(28, 65, 30.4f);
+            weatherData.SetMeasurements(29, 70, 29.2f);
+            weatherData.SetMeasurements(30, 90, 29.2f);
+
+            weatherData.RemoveObserver(forecastDisplay);
+
+            weatherData.SetMeasurements(25, 60, 30.0f);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
